948c9f15b3211f5638dcc75d4873465bbd295e93
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2023-09-29 12:01:09 -0500

    Add cors

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ from typing import Annotated, List
 
 import jwt
 from fastapi import FastAPI, Header, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class User(BaseModel):
